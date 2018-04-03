b628b73df66a9c0c553cee469784fd28ec89cfff
apollo_b <apollo.board@gmail.com>
2018-04-02 20:21:34 -0500

    Lesson 105 of 189: redux store created; fleshing out dashboard, add/edit components and connecting them to store.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+const water = store.dispatch(addExpense({description: 'Water bill', amount: 12000}));
+const gas = store.dispatch(addExpense({description: 'Gas bill', amount: 3000, createdAt: 1000}));
+store.dispatch(addExpense({description: 'Rent', amount: 73000}));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('gas'));
+}, 3000)
+
+console.log(store.getState());
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+
+ReactDOM.render(jsx, document.getElementById('app'));
