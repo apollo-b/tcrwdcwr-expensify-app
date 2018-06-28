fc0971d0f4f3807b986b7425e68be490c3c1bd20
apollo_b <apollo.board@gmail.com>
2018-06-27 19:10:34 -0500

    Lecture 143 completed. Practice basic update of Firebase database values.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,8 +11,26 @@ const config = {
 
   firebase.initializeApp(config);
 
-  firebase.database().ref().set(
+  const database = firebase.database();
+
+  database.ref().set(
     {
-        name: 'Apollo Board'
+        name: 'Apollo Board',
+        age: 39,
+        isSingle: true,
+        location: {
+            city: 'St. Louis',
+            country: 'United States'
+        }
     }
 );
+
+// database.ref().set("This is my data");
+
+database.ref('age').set(27);
+database.ref('location/city').set('Nyuu Yooku');
+
+database.ref('attributes').set({
+    height: 66,
+    weight: 185
+});
