02295b91028e6567eac56784e3d4024dcbee89d3
apollo_b <apollo.board@gmail.com>
2018-06-27 22:09:37 -0500

    Lecture 147 completed. Continued Firebase CRUD additions - 'remove'.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,32 +13,23 @@ const config = {
 
   const database = firebase.database();
 
-  database.ref().set(
-    {
-        name: 'Apollo Board',
-        age: 39,
-        isSingle: true,
-        location: {
-            city: 'St. Louis',
-            country: 'United States'
-        }
-    }
-).then(()=>{
-    console.log('Data is saved.');
-}).catch((e)=>{
-    console.log('This failed.', e)
-});
+//   database.ref().set(
+//     {
+//         name: 'Apollo Board',
+//         age: 39,
+//         isSingle: true,
+//         location: {
+//             city: 'St. Louis',
+//             country: 'United States'
+//         }
+//     }
+// ).then(()=>{
+//     console.log('Data is saved.');
+// }).catch((e)=>{
+//     console.log('This failed.', e)
+// });
 
-// database.ref().set("This is my data");
-
-// database.ref('age').set(27);
-// database.ref('location/city').set('Nyuu Yooku');
-
-database.ref('attributes').set({
-    height: 66,
-    weight: 185
-}).then(() => {
-    console.log('Updated database 1.');
-}).catch((e) => {
-    console.log("Failed to update database.\n", e);
-});
+database.ref('isSingle')
+    .remove()
+    .then(() => console.log('Value removed 1.'))
+    .catch((e)=> console.log('No data removed.', e));
