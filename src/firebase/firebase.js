2e447301b18e4926a96ab58273e05de847ff0ebe
apollo_b <apollo.board@gmail.com>
2018-07-16 12:43:57 -0500

    Lecture 147 of 189 completed. Practicing updating data in Firebase database.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,23 +13,37 @@ const config = {
 
   const database = firebase.database();
 
-//   database.ref().set(
-//     {
-//         name: 'Apollo Board',
-//         age: 39,
-//         isSingle: true,
-//         location: {
-//             city: 'St. Louis',
-//             country: 'United States'
-//         }
-//     }
-// ).then(()=>{
-//     console.log('Data is saved.');
-// }).catch((e)=>{
-//     console.log('This failed.', e)
-// });
+  database.ref().set(
+    {
+        name: 'Apollo Board',
+        age: 39,
+        stressLevel: 6,
+        job: {
+          title: 'Software Developer',
+          company: 'Google'
+        },
+        isSingle: true,
+        location: {
+            city: 'St. Louis',
+            country: 'United States'
+        }
+    }
+).then(()=>{
+    console.log('Data is saved.');
+}).catch((e)=>{
+    console.log('This failed.', e)
+});
 
-database.ref('isSingle')
-    .remove()
-    .then(() => console.log('Value removed 1.'))
-    .catch((e)=> console.log('No data removed.', e));
+// Change the stresslevel to a 9
+// Change the job.company to Amazon
+// Change location.city to Seattle
+database.ref().update({
+  stressLevel: 9,
+  'job/company': 'Amazon',
+  'location/city': 'Seattle'
+});
+
+// database.ref('isSingle')
+//     .remove()
+//     .then(() => console.log('Value removed 1.'))
+//     .catch((e)=> console.log('No data removed.', e));
