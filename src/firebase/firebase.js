a50f615c758470b675a1b1817bcb175db38f8d20
apollo_b <apollo.board@gmail.com>
2018-07-18 12:41:17 -0500

    Lecture 148 of 189 completed. Practiced fetching data from database using one-time and subscription processes.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,35 +13,68 @@ const config = {
 
   const database = firebase.database();
 
-  database.ref().set(
-    {
-        name: 'Apollo Board',
-        age: 39,
-        stressLevel: 6,
-        job: {
-          title: 'Software Developer',
-          company: 'Google'
-        },
-        isSingle: true,
-        location: {
-            city: 'St. Louis',
-            country: 'United States'
-        }
-    }
-).then(()=>{
-    console.log('Data is saved.');
-}).catch((e)=>{
-    console.log('This failed.', e)
-});
+  // set up data subscription
+  // Andrew is a Software Developer at Amazon
+  // Change the data and reprint
 
-// Change the stresslevel to a 9
-// Change the job.company to Amazon
-// Change location.city to Seattle
-database.ref().update({
-  stressLevel: 9,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-});
+  database.ref().on('value', (snapshot) => {
+    const obj = snapshot.val();
+    console.log(`${obj.name} is a ${obj.job.title} at ${obj.job.company}.`);
+    console.log("time", "other");
+  });
+  setTimeout(() => database.ref('job/company').set('Amazon'), 3500);
+
+//   const onValueChange = database.ref().on('value', (snapshot) => {
+//         console.log(snapshot.val());
+//   });
+  
+//   setTimeout(() => {
+//     database.ref('age').set(39);
+//   }, 3500);
+//   setTimeout(() => {
+//     database.ref().off(onValueChange);
+//   }, 7000);
+//   setTimeout(() => {
+//     database.ref('age').set(40);
+//   }, 10500);
+//   database.ref()
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   }).catch((e) => {
+//     console.log('Error fetching data', e);
+//   });
+
+//   database.ref().set(
+//     {
+//         name: 'Apollo Board',
+//         age: 39,
+//         stressLevel: 6,
+//         job: {
+//           title: 'Software Developer',
+//           company: 'Google'
+//         },
+//         isSingle: true,
+//         location: {
+//             city: 'St. Louis',
+//             country: 'United States'
+//         }
+//     }
+// ).then(()=>{
+//     console.log('Data is saved.');
+// }).catch((e)=>{
+//     console.log('This failed.', e)
+// });
+
+// // Change the stresslevel to a 9
+// // Change the job.company to Amazon
+// // Change location.city to Seattle
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
 
 // database.ref('isSingle')
 //     .remove()
