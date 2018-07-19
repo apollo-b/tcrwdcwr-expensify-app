44f377c1c1ed5a297244338425f57ccf57e4204f
apollo_b <apollo.board@gmail.com>
2018-07-19 12:14:31 -0500

    Lecture 149 of 189 completed. Working with array data in Firebase. Push, update.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,70 +13,35 @@ const config = {
 
   const database = firebase.database();
 
-  // set up data subscription
-  // Andrew is a Software Developer at Amazon
-  // Change the data and reprint
+  // Setup 'expenses' with three items (desc, note, amount, createdAt)
+  // Set up with 3 push calls
+  const expenses = [{
+      desc: "item 1",
+      note: "note 1",
+      amount: 11,
+      createdAt: 111,
+  }, {
+    desc: "item 2",
+    note: "note 2",
+    amount: 22,
+    createdAt: 222
+}, {
+    desc: "item 3",
+    note: "note 3",
+    amount: 33,
+    createdAt: 333
+}];
+database.ref('notes').set(null);
+database.ref('notes').push(expenses[0]);
+database.ref('notes').push(expenses[1]);
+database.ref('notes').push(expenses[2]);
 
-  database.ref().on('value', (snapshot) => {
-    const obj = snapshot.val();
-    console.log(`${obj.name} is a ${obj.job.title} at ${obj.job.company}.`);
-    console.log("time", "other");
-  });
-  setTimeout(() => database.ref('job/company').set('Amazon'), 3500);
 
-//   const onValueChange = database.ref().on('value', (snapshot) => {
-//         console.log(snapshot.val());
-//   });
-  
-//   setTimeout(() => {
-//     database.ref('age').set(39);
-//   }, 3500);
-//   setTimeout(() => {
-//     database.ref().off(onValueChange);
-//   }, 7000);
-//   setTimeout(() => {
-//     database.ref('age').set(40);
-//   }, 10500);
-//   database.ref()
-//   .once('value')
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   }).catch((e) => {
-//     console.log('Error fetching data', e);
+//   database.ref('notes').push({
+//       title: 'Course Topics',
+//       body: 'React Native, Angular, Python'
 //   });
 
-//   database.ref().set(
-//     {
-//         name: 'Apollo Board',
-//         age: 39,
-//         stressLevel: 6,
-//         job: {
-//           title: 'Software Developer',
-//           company: 'Google'
-//         },
-//         isSingle: true,
-//         location: {
-//             city: 'St. Louis',
-//             country: 'United States'
-//         }
-//     }
-// ).then(()=>{
-//     console.log('Data is saved.');
-// }).catch((e)=>{
-//     console.log('This failed.', e)
-// });
-
-// // Change the stresslevel to a 9
-// // Change the job.company to Amazon
-// // Change location.city to Seattle
-// database.ref().update({
-//   stressLevel: 9,
-//   'job/company': 'Amazon',
-//   'location/city': 'Seattle'
-// });
-
-// database.ref('isSingle')
-//     .remove()
-//     .then(() => console.log('Value removed 1.'))
-//     .catch((e)=> console.log('No data removed.', e));
+//   database.ref('notes/-LHiYnEkfNMLuqSgdo1V').update({
+//       body: 'Whatttt'
+//   });
